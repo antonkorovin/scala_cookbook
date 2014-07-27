166cd0586bce848bcf28b18041f23d57816a85f6
Anton Korovin <korovin.anton@gmail.com>
2014-07-27 08:17:47 +0300

    Added simple regexp test.

--- src/test/scala/org/ak/scala/cookbook/ch01/StringRegexp.scala
+++ src/test/scala/org/ak/scala/cookbook/ch01/StringRegexp.scala
@@ -1,9 +1,22 @@
 package org.ak.scala.cookbook.ch01
 
+import org.scalatest.{Matchers, FunSuite}
+
 /**
  * @author antonk
  * @since  7/26/14 - 11:56 PM
  */
-class StringRegexp {
+class StringRegexp extends FunSuite with Matchers {
+    test("creating regexp with r postfix") {
+      val pattern = "^([^=]+)=([^=]+)$".r
+      val str = "key=value"
+
+      val matches = pattern.findAllIn(str)
+
+      matches.next()
+      matches.groupCount shouldEqual 2
+      matches.group(1) shouldEqual "key"
+      matches.group(2) shouldEqual "value"
+    }
 
 }

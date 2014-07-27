1c98715c831ef8c9d9d11701e1927477e728a7e7
Anton Korovin <korovin.anton@gmail.com>
2014-07-27 08:27:18 +0300

    Added simple test for processing string with map method.

---
+++ src/test/scala/org/ak/scala/cookbook/ch01/ProcessingStringByChar.scala
@@ -0,0 +1,16 @@
+package org.ak.scala.cookbook.ch01
+
+import org.scalatest.{Matchers, FunSuite}
+
+/**
+ * @author antonk
+ * @since  7/27/14 - 8:23 AM
+ */
+class ProcessingStringByChar extends FunSuite with Matchers {
+  test("using map") {
+    val str = "hello"
+    val capitalized = str.map(_.toUpper)
+
+    capitalized shouldEqual "HELLO"
+  }
+}

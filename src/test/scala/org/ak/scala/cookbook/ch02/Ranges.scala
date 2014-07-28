fbd5cf6e9c163823f5299ffc81477ba68f322a4a
Anton Korovin <korovin.anton@gmail.com>
2014-07-28 22:46:39 +0300

    Added test for ranges.

---
+++ src/test/scala/org/ak/scala/cookbook/ch02/Ranges.scala
@@ -0,0 +1,11 @@
+package org.ak.scala.cookbook.ch02
+
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+ * @author antonk
+ * @since  7/28/14 - 10:43 PM
+ */
+class Ranges extends FunSuite with Matchers {
+
+}

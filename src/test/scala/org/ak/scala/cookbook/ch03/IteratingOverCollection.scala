4664fbd762e66dc265d364a8005bc13c090a528d
Anton Korovin <korovin.anton@gmail.com>
2014-07-29 23:25:43 +0300

    Added tests for comprehensions and guards.

--- src/test/scala/org/ak/scala/cookbook/ch03/IteratingOverCollection.scala
+++ src/test/scala/org/ak/scala/cookbook/ch03/IteratingOverCollection.scala
@@ -8,7 +8,7 @@ import org.scalatest.{Matchers, FunSuite}
  */
 class IteratingOverCollection extends FunSuite with Matchers {
   test("iterating with for") {
-    val arr = Array(1, 2, 3, 4, 5)
+    val arr = (1 to 5).toArray
 
     var sum = 0
     for (element <- arr) {
@@ -22,7 +22,7 @@ class IteratingOverCollection extends FunSuite with Matchers {
 
 
   test("iterating with for with indexes") {
-    val arr = Array(1, 2, 3, 4, 5)
+    val arr = (1 to 5).toArray
 
     var sum = 0
     for (index <- 0 until arr.length) {
@@ -36,12 +36,35 @@ class IteratingOverCollection extends FunSuite with Matchers {
 
 
   test("iterating with foreach") {
-    val arr = Array(1, 2, 3, 4, 5)
+    val arr = (1 to 5).toArray
 
     var sum = 0
     arr.foreach(sum += _)
 
 
     sum shouldEqual arr.sum
+  }
+
+
+  test("for comprehensions") {
+    val arr = (1 to 5).toArray
+    val expectedArray = Array("1", "2", "3", "4", "5")
+
+    val strArr = for (element <- arr) yield element.toString
+
+    strArr shouldEqual expectedArray
+  }
+
+
+  test("for with guards") {
+    val arr = (1 to 10).toArray
+    val expectedArray = Array("2", "4", "6", "8", "10")
+
+    val strArr = for {
+      element <- arr
+      if element % 2 == 0
+    } yield element.toString
+
+    strArr shouldEqual expectedArray
   }
 }

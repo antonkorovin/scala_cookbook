2ba93d6f3262e70bee56a3ef4da5dda494cb6b73
Anton Korovin <korovin.anton@gmail.com>
2014-08-02 21:01:33 +0300

    Added empty test for exceptions

---
+++ src/test/scala/org/ak/scala/cookbook/ch03/ExceptionsAndMatchExpressions.scala
@@ -0,0 +1,11 @@
+package org.ak.scala.cookbook.ch03
+
+import org.scalatest.{Matchers, FunSuite}
+
+/**
+ * @author antonk
+ * @since  8/2/14 - 8:59 PM
+ */
+class ExceptionsAndMatchExpressions extends FunSuite with Matchers {
+
+}

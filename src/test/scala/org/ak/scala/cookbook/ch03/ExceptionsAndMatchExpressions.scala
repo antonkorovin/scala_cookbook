5b02102f20b420a9eb3ac08453cbc86ddbb3fb53
Anton Korovin <korovin.anton@gmail.com>
2014-08-02 21:04:29 +0300

    Adde test for throw-catch.

--- src/test/scala/org/ak/scala/cookbook/ch03/ExceptionsAndMatchExpressions.scala
+++ src/test/scala/org/ak/scala/cookbook/ch03/ExceptionsAndMatchExpressions.scala
@@ -7,5 +7,14 @@ import org.scalatest.{Matchers, FunSuite}
  * @since  8/2/14 - 8:59 PM
  */
 class ExceptionsAndMatchExpressions extends FunSuite with Matchers {
+  test("throw and catch") {
+    val e = new Exception("Expected")
 
+    try {
+      throw e
+    } catch {
+      case exc: Exception =>
+        exc should be theSameInstanceAs e
+    }
+  }
 }

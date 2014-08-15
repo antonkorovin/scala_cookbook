28944ce8efb981fa802eb8c77143807dcd7e5b11
Anton Korovin <korovin.anton@gmail.com>
2014-08-15 12:51:03 +0300

    Added test for calling a method on a superclass.

---
+++ src/test/scala/org/ak/scala/cookbook/ch05/MethodsTest.scala
@@ -0,0 +1,55 @@
+package org.ak.scala.cookbook.ch05
+
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+ * @author antonk
+ * @since  8/15/14 - 12:37 PM
+ */
+class MethodsTest
+  extends FunSuite
+          with Matchers
+          with GeneratorDrivenPropertyChecks {
+
+  test("calling a method on a superclass") {
+    trait BaseMember {
+      def role = "NoRole"
+    }
+    trait Member {
+      def role= "MemberRole"
+    }
+    trait Mother extends Member {
+      override def role = "MotherRole"
+    }
+    trait Father extends Member {
+      override def role = "FatherRole"
+    }
+
+    class Child
+      extends Member
+              with Mother
+              with Father {
+      override def role = "ChildRole"
+
+      // def baseRole = super[BaseMember].role
+      // Compile error: BaseMember does not name
+      // a parent class of class Child
+      // to avoid this add "with BaseMember"
+
+      def memberRole = super[Member].role
+      def motherRole = super[Mother].role
+      def fatherRole = super[Father].role
+    }
+
+
+    // ////////////////////////////////
+
+
+    val child = new Child
+    child.role shouldEqual       "ChildRole"
+    child.motherRole shouldEqual "MotherRole"
+    child.fatherRole shouldEqual "FatherRole"
+    child.memberRole shouldEqual "MemberRole"
+  }
+}

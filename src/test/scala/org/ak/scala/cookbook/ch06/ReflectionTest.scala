fb05a922eb2d2ae907fdb22dc325654ae8a3fa24
Anton Korovin <korovin.anton@gmail.com>
2014-08-20 18:28:51 +0300

    Added test for object casting.

---
+++ src/test/scala/org/ak/scala/cookbook/ch06/ReflectionTest.scala
@@ -0,0 +1,25 @@
+package org.ak.scala.cookbook.ch06
+
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+ * @author antonk
+ * @since  8/20/14 - 6:24 PM
+ */
+class ReflectionTest
+  extends FunSuite
+          with Matchers {
+
+
+  test("object casting") {
+    val intAsAny: Any = 42
+    val intValue: Int = intAsAny.asInstanceOf[Int]
+
+    intValue shouldEqual 42
+
+
+    intercept[ClassCastException] {
+      intAsAny.asInstanceOf[String]
+    }
+  }
+}

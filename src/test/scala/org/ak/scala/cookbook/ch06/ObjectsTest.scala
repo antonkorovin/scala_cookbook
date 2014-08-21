0f4b9ea1c2fbbe6be936dc91d29e2b4c8f2cae44
Anton Korovin <korovin.anton@gmail.com>
2014-08-21 13:11:28 +0300

    Added test for launching an application with an object.

---
+++ src/test/scala/org/ak/scala/cookbook/ch06/ObjectsTest.scala
@@ -0,0 +1,33 @@
+package org.ak.scala.cookbook.ch06
+
+import java.io.{File, FileOutputStream, PrintWriter}
+
+import org.scalatest.{FunSuite, Matchers}
+
+import scala.io.Source
+
+/**
+ * @author antonk
+ * @since  8/21/14 - 12:32 PM
+ */
+class ObjectsTest
+  extends FunSuite
+          with Matchers {
+
+  val tmpFile = File.createTempFile("ScalaCookbook", "ObjectsTest")
+  val outFile = new PrintWriter(new FileOutputStream(tmpFile))
+
+  object Main extends App {
+    outFile.println(args.map(_.toInt).sum)
+    outFile.close()
+  }
+
+
+  test("launching an application with an object") {
+    Main.main(Array("1", "2", "3", "4", "5"))
+
+
+    val lines = Source.fromFile(tmpFile).getLines().toList
+    lines shouldEqual List("15")
+  }
+}

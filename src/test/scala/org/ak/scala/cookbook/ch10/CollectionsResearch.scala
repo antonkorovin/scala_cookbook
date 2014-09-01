4c2e84019123aa79de1584100aa108c73e3062b9
Anton Korovin <korovin.anton@gmail.com>
2014-09-01 20:22:02 +0300

    Added test suite for collections research.

---
+++ src/test/scala/org/ak/scala/cookbook/ch10/CollectionsResearch.scala
@@ -0,0 +1,17 @@
+package org.ak.scala.cookbook.ch10
+
+import org.scalatest.{Matchers, FunSuite}
+
+/**
+ * @author antonk
+ * @since  9/1/14 - 8:17 PM
+ */
+class CollectionsResearch
+  extends FunSuite
+          with Matchers {
+
+  test("understanding the collections hierarchy") {
+    pending
+  }
+
+}

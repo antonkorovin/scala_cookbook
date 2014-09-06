3071c4b82b5c1d2c06557d8a3bef0b3c51bf1d9b
Anton Korovin <korovin.anton@gmail.com>
2014-09-06 21:28:19 +0300

    Added test suite for choosing collection class.

---
+++ src/test/scala/org/ak/scala/cookbook/ch10/ChoosingCollectionClassResearch.scala
@@ -0,0 +1,12 @@
+package org.ak.scala.cookbook.ch10
+
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+ * @author antonk
+ * @since  9/4/14 - 1:07 PM
+ */
+class ChoosingCollectionClassResearch
+  extends FunSuite
+          with Matchers {
+}

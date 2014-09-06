55c65a397e227d439a1321d494c02d865c5082d2
Anton Korovin <korovin.anton@gmail.com>
2014-09-06 22:08:41 +0300

    Added test for choosing an indexed immutable sequence.

--- src/test/scala/org/ak/scala/cookbook/ch10/ChoosingCollectionClassResearch.scala
+++ src/test/scala/org/ak/scala/cookbook/ch10/ChoosingCollectionClassResearch.scala
@@ -9,4 +9,34 @@ import org.scalatest.{FunSuite, Matchers}
 class ChoosingCollectionClassResearch
   extends FunSuite
           with Matchers {
+
+  test("choosing an indexed immutable sequence") {
+    val vec = Vector(2, 4, 8, 16, 32)
+
+    // TODO: Check performance
+    vec(3) shouldEqual 16
+
+    // TODO: Check performance
+    // Uses `apply(0)`
+    vec.head shouldEqual 2
+
+    // TODO: Check performance
+    // Uses `drop(1)`
+    vec.tail shouldEqual Vector(4, 8, 16, 32)
+
+
+    // Can't use like this `vec(2) = 42`
+    // Instead use:
+    val updated = vec.updated(2, 42)
+
+    vec(2) shouldEqual 8
+    updated(2) shouldEqual 42
+
+    val appended = vec :+ 42
+    val prepended = 42 +: vec
+
+    vec shouldEqual Vector(2, 4, 8, 16, 32)
+    appended shouldEqual Vector(2, 4, 8, 16, 32, 42)
+    prepended shouldEqual Vector(42, 2, 4, 8, 16, 32)
+  }
 }

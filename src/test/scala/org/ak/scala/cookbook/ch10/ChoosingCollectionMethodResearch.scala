a96de3be867cd85f3142d0b990ee2521b8a235c3
Anton Korovin <korovin.anton@gmail.com>
2014-09-23 23:03:41 +0300

    Added structure for p10.3 research.

--- src/test/scala/org/ak/scala/cookbook/ch10/ChoosingCollectionMethodResearch.scala
+++ src/test/scala/org/ak/scala/cookbook/ch10/ChoosingCollectionMethodResearch.scala
@@ -1,9 +1,23 @@
 package org.ak.scala.cookbook.ch10
 
+import org.scalatest.{FunSuite, Matchers}
+
 /**
  * @author antonk
  * @since  9/23/14 - 10:59 PM
  */
-class ChoosingCollectionMethodResearch {
-  // TODO 10.3. Choosing a Collection Method to Solve a Problem
+class ChoosingCollectionMethodResearch
+  extends FunSuite
+          with Matchers {
+
+  // TODO Common collection methods
+
+
+  // TODO Mutable collection methods
+
+
+  // TODO Immutable collection operators
+
+
+  // TODO Maps
 }

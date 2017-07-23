c4500590256ab93cf7a471c19e12601704659836
Anton Korovin <korovin.anton@gmail.com>
2017-07-24 00:54:23 +0300

    [performance] Removed MethodsUnderTest
    Simplified 'measure*' methods.

--- src/test/scala/org/ak/scala/cookbook/ch10/util/MeasureHeadAndTail.scala
+++ src/test/scala/org/ak/scala/cookbook/ch10/util/MeasureHeadAndTail.scala
@@ -1,16 +1,15 @@
 package org.ak.scala.cookbook.ch10.util
 
-import org.ak.scala.cookbook.ch10.CollectionPerformanceResearch.{lists, using}
+import org.ak.scala.cookbook.ch10.CollectionPerformanceResearch.using
 import org.scalameter.api.Gen
-
-import scala.language.reflectiveCalls
 
 /**
   * @author antonk
   * @since 7/25/17 - 1:55 PM
   */
-trait MeasureHeadAndTail extends MethodsUnderTestAware {
-  def measureHeadAndTailFor(gen: Gen[_ <: HeadAware[_] with TailAware[_]]) = {
+trait MeasureHeadAndTail {
+
+  def measureHeadAndTailFor(gen: Gen[_ <: Traversable[_]]) = {
     using(gen) curve "head" in {
       _.head
     }
--- src/test/scala/org/ak/scala/cookbook/ch10/util/MeasureSize.scala
+++ src/test/scala/org/ak/scala/cookbook/ch10/util/MeasureSize.scala
@@ -3,15 +3,13 @@ package org.ak.scala.cookbook.ch10.util
 import org.ak.scala.cookbook.ch10.CollectionPerformanceResearch.using
 import org.scalameter.api.Gen
 
-import scala.language.reflectiveCalls
-
 /**
   * @author antonk
   * @since 7/25/17 - 12:29 PM
   */
-trait MeasureSize extends MethodsUnderTestAware {
+trait MeasureSize {
 
-  def measureSizeFor(gen: Gen[_ <: SizeAware]) = {
+  def measureSizeFor(gen: Gen[_ <: Traversable[_]]) = {
     using(gen) curve "size" in {
       _.size
     }

8dd33fc99d76f35fd3ce7e90490009e7ab42c2ac
Anton Korovin <korovin.anton@gmail.com>
2017-08-01 08:49:57 +0300

    [performance] Add generators for mutable sequences.

---
+++ src/test/scala/org/ak/scala/cookbook/ch10/util/gen/MutableCollections.scala
@@ -0,0 +1,59 @@
+package org.ak.scala.cookbook.ch10.util.gen
+
+import org.scalameter.Gen.Collections
+
+import scala.collection.mutable
+
+/**
+  * @author antonk
+  * @since 8/8/17 - 6:22 PM
+  */
+trait MutableCollections extends Collections {
+  override val arraybuffers = super.arraybuffers.cached
+
+  val listbuffers = {
+    for {
+      size <- sizes
+    } yield mutable.ListBuffer(0 until size: _*)
+  }.cached
+
+  val stringbuilders = {
+    for {
+      size <- sizes
+    } yield {
+      new mutable.StringBuilder("*" * size)
+    }
+  }.cached
+
+
+  val mutablelists = {
+    for {
+      size <- sizes
+    } yield mutable.MutableList(0 until size: _*)
+  }.cached
+
+  val mutablequeues = {
+    for {
+      size <- sizes
+    } yield mutable.Queue(0 until size: _*)
+  }.cached
+
+  val arrayseqs = {
+    for {
+      size <- sizes
+    } yield mutable.ArraySeq(0 until size: _*)
+  }.cached
+
+  val mutablestacks = {
+    for {
+      size <- sizes
+    } yield mutable.Stack(0 until size: _*)
+  }.cached
+
+
+  val arraystacks = {
+    for {
+      size <- sizes
+    } yield mutable.ArrayStack(0 until size: _*)
+  }.cached
+}

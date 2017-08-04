17575040be47e8d582cffbee2a1038b4b68457b8
Anton Korovin <korovin.anton@gmail.com>
2017-08-05 01:15:16 +0300

    [performance] Measure 'min' and 'size' accepts TraversableLike

--- src/test/scala/org/ak/scala/cookbook/ch10/util/measure/MeasureMin.scala
+++ src/test/scala/org/ak/scala/cookbook/ch10/util/measure/MeasureMin.scala
@@ -3,12 +3,14 @@ package org.ak.scala.cookbook.ch10.util.measure
 import org.ak.scala.cookbook.ch10.CollectionPerformanceResearch.using
 import org.scalameter.api.Gen
 
+import scala.collection.TraversableLike
+
 /**
   * @author antonk
   * @since 8/9/17 - 5:28 PM
   */
 trait MeasureMin {
-  def measureMinFor[T](gen: Gen[_ <: Traversable[T]])(implicit ordering: Ordering[T]) = {
+  def measureMinFor[T](gen: Gen[_ <: TraversableLike[T, _]])(implicit ordering: Ordering[T]) = {
     using(gen) curve "min" in {
       _.min
     }
--- src/test/scala/org/ak/scala/cookbook/ch10/util/measure/MeasureSize.scala
+++ src/test/scala/org/ak/scala/cookbook/ch10/util/measure/MeasureSize.scala
@@ -3,13 +3,15 @@ package org.ak.scala.cookbook.ch10.util.measure
 import org.ak.scala.cookbook.ch10.CollectionPerformanceResearch.using
 import org.scalameter.api.Gen
 
+import scala.collection.TraversableLike
+
 /**
   * @author antonk
   * @since 7/25/17 - 12:29 PM
   */
 trait MeasureSize {
 
-  def measureSizeFor(gen: Gen[_ <: Traversable[_]]) = {
+  def measureSizeFor(gen: Gen[_ <: TraversableLike[_, _]]) = {
     using(gen) curve "size" in {
       _.size
     }
